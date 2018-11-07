43104841c31caa40922b47cb39dfff09d73b6dbc
Arjan Regmi <ar4752@truman.edu>
2018-11-06 21:33:47 -0600

    class

---
+++ BinaryTree.java
@@ -0,0 +1,13 @@
+class BinaryTree
+{
+	public int x; 
+	public BinaryTree l; 
+	public BinaryTree r; 
+	
+	public BinaryTree(int x, BinaryTree l, BinaryTree r)
+	{
+		this.x = x; 
+		this.l = l;
+		this.r = r; 
+	}
+}
